77bd244e2cdbccafa1e4acc70ccbae56a025ce5b
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2020-03-03 20:56:27 -0500

    Rename project, add suit.cs and rank.cs

---
+++ Card_Lib/Rank.cs
@@ -0,0 +1,32 @@
+﻿/**Rank.cs - rank enum for set card rank
+ * 
+ * Author: Group #2
+ * Since : 2020 - 03 - 03
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch13CardLib
+{
+    /// <summary>
+    /// Rank Enum, for set card rank
+    /// </summary>
+    public enum Rank
+    {
+        Ace = 1,
+        Deuce,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King
+    }
+}
---
+++ Card_Lib/Suit.cs
@@ -0,0 +1,23 @@
+﻿/**Suit.cs - enum for card suit
+ * 
+ * Author: Group #2
+ * Since : 2020 - 03 - 03
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch13CardLib
+{
+    /// <summary>
+    /// Suit enum for card suit
+    /// </summary>
+    public enum Suit
+    {
+        Club,
+        Diamond,
+        Heart,
+        Spade
+    }
+}
