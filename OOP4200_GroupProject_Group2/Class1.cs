e1d460fce39092ac8e4b80529f02364420daacef
GabrielNathanLegawa <gabrielnathan.legawa@dcmail.ca>
2020-03-03 20:36:37 -0500

    Add project files.

---
+++ OOP4200_GroupProject_Group2/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP4200_GroupProject_Group2
+{
+    public class Class1
+    {
+    }
+}
